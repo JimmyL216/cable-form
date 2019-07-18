0f4c95ba593aa74add50e101745d8156c4c877de
Jimmy <jimmy@MacBookPro-2.local>
2019-07-18 13:43:23 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// function showTextbox() {
+//   var input = document.getElementById('em_pow_input')
+//   if (input.style.display === "none") {
+//     input.style.display = "block";
+//   } else {
+//     input.style.display = "none";
+//   }
+// }
+
+function showTextbox() {
+  document.getElementById('em_pow_input').style.display = "block";
+}
+
+function hideTextbox() {
+  document.getElementById('em_pow_input').style.display = "none";
+
+}
+
+function showTextboxA() {
+  document.getElementById('access_input').style.display = "block";
+}
+
+function hideTextboxA() {
+  document.getElementById('access_input').style.display = "none";
+
+}
