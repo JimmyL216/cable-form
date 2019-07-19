c6608e900bc1bd99fd5155fa5442f5740b37e6af
Jimmy <jimmy@MacBookPro-2.local>
2019-07-19 11:09:41 -0400

    Finishes the form

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+function show_em_pow() {
+  var c = document.getElementById("em_pow").children;
+  c[7].style.display = "block";
+}
+
+function hide_em_pow() {
+  var c = document.getElementById("em_pow").children;
+  c[7].style.display = "none";
+}
+
+function show_shared() {
+  var c = document.getElementById("shared").children;
+  c[5].style.display = "block";
+}
+
+function hide_shared() {
+  var c = document.getElementById("shared").children;
+  c[5].style.display = "none";
+}
+
+function show_access() {
+  var c = document.getElementById("access").children;
+  c[5].style.display = "block";
+}
+
+function hide_access() {
+  var c = document.getElementById("access").children;
+  c[5].style.display = "none";
+}
+
+function show_extended_oth() {
+  var c = document.getElementById("extended_oth").children;
+  c[8].style.display = "block";
+}
+
+function hide_extended_oth() {
+  var c = document.getElementById("extended_oth").children;
+  c[8].style.display = "none";
+}
+
+
+
+
+
+
 // function showTextbox() {
 //   var input = document.getElementById('em_pow_input')
 //   if (input.style.display === "none") {
@@ -7,20 +52,38 @@
 //   }
 // }
 
-function showTextbox() {
-  document.getElementById('em_pow_input').style.display = "block";
-}
 
-function hideTextbox() {
-  document.getElementById('em_pow_input').style.display = "none";
 
-}
+//
+// function showTextbox() {
+//   document.getElementById('em_pow_input').style.display = "block";
+// }
+//
+// function hideTextbox() {
+//   document.getElementById('em_pow_input').style.display = "none";
+//
+// }
+//
+// function showTextboxA() {
+//   document.getElementById('access_input').style.display = "block";
+// }
+//
+// function hideTextboxA() {
+//   document.getElementById('access_input').style.display = "none";
+// }
+//
+//
+//
 
-function showTextboxA() {
-  document.getElementById('access_input').style.display = "block";
-}
 
-function hideTextboxA() {
-  document.getElementById('access_input').style.display = "none";
 
-}
+// function show() {
+//   // this.parentElement.querySelector(".extended").style.display = "block";
+//   this.parentElement;
+//   console.log(this.parentElement);
+// }
+
+// function hide() {
+//   document.getelemntbyclassName("show").nextElementSibling("show").style.display = "none";
+//
+// }
